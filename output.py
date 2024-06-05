c99c8a51d35211bdf8138a7d699deaaf9836734f
Aleksander Wieliński <a.wielinski@student.uw.edu.pl>
2024-06-05 22:26:53 +0200

    output amended / to fix heatmap

--- output.py
+++ output.py
@@ -1,36 +1,38 @@
 import pandas as pd
 from ML_classifier import MLClassifier
 
-# Load the preprocessed data
-data_path = 'data/preprocessed_data.csv'
-preprocessed_I75_data = pd.read_csv(data_path, low_memory=False)
+# # Load the preprocessed data
+# data_path = 'data/preprocessed_data.csv'
+# preprocessed_I75_data = pd.read_csv(data_path, low_memory=False)
 
-# Subset the data for testing or demonstration
-preprocessed_I75_data_short = preprocessed_I75_data[:1100]
+# # Subset the data for testing or demonstration
+# preprocessed_I75_data_short = preprocessed_I75_data[:1100]
 
-# Initialize the classifier with the subset data
-classifier = MLClassifier(data=preprocessed_I75_data_short, target='incident at sensor (i)')
-results = classifier.calculate_metrics_for_combinations()
+def output(data):
 
-#DR
-classifier.generate_heatmap(results, 'DR', setting=1)  
-classifier.generate_heatmap(results, 'DR', setting=2)  
+    # Initialize the classifier with the subset data
+    classifier = MLClassifier(data=data, target='incident at sensor (i)')
+    results = classifier.calculate_metrics_for_combinations()
 
-#FAR
-classifier.generate_heatmap(results, 'FAR', setting=1) 
-classifier.generate_heatmap(results, 'FAR', setting=2)  
+    #DR
+    classifier.generate_heatmap(results, 'DR', setting=1)  
+    classifier.generate_heatmap(results, 'DR', setting=2)  
 
-#AUC-ROC
-classifier.generate_heatmap(results, 'AUC-ROC', setting=1)  
-classifier.generate_heatmap(results, 'AUC-ROC', setting=2)  
+    #FAR
+    classifier.generate_heatmap(results, 'FAR', setting=1) 
+    classifier.generate_heatmap(results, 'FAR', setting=2)  
 
-#AUC-PR
-classifier.generate_heatmap(results, 'AUC-PR', setting=1)  
-classifier.generate_heatmap(results, 'AUC-PR', setting=2)  
+    #AUC-ROC
+    classifier.generate_heatmap(results, 'AUC-ROC', setting=1)  
+    classifier.generate_heatmap(results, 'AUC-ROC', setting=2)  
 
-#SHAP plot values
-classifier.plot_shap_values(model_name='XGBoost') 
+    #AUC-PR
+    classifier.generate_heatmap(results, 'AUC-PR', setting=1)  
+    classifier.generate_heatmap(results, 'AUC-PR', setting=2)  
 
-#Summary table
-df_summary = classifier.generate_summary_table(results)
-print(df_summary)
+    #SHAP plot values
+    classifier.plot_shap_values(model_name='XGBoost') 
+
+    #Summary table
+    df_summary = classifier.generate_summary_table(results)
+    print(df_summary)
